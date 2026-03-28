303110957e744680b556ed6b7df8606659f23592
purush2300 <reddypurushothm@gmail.com>
2026-03-28 22:38:32 +0530

    Initial commit

--- Rec1.java
+++ Rec1.java
@@ -1,16 +1,18 @@
 public class Rec1 {
-    static int fun(int a){
-        if(a==1){
-            return 1;
+    static int fun(int i){
+        
+        if(i==0){
+            return 0;
         }
        
-        return a*fun(a-1);
+        return fun(i/10)+i%10;
        
         
     }
     public static void main(String[] args) {
-        int a=5;
-        int j=fun(a);
-         System.out.println(j);
+        
+        int i=12345;
+        System.out.println(fun(i));
+       
     }
 }
