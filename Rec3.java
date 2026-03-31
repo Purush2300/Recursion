aa8b7d8ff621be5e81d440158b1cc3e570aa179a
purush2300 <reddypurushothm@gmail.com>
2026-03-31 22:08:24 +0530

    Initial commit

--- Rec3.java
+++ Rec3.java
@@ -3,13 +3,21 @@ public class Rec3 {
   int[][] matrix = {
     {1, 2, 3,4},
     { 5, 6,7,8},
-    {8,9,10,11},
+    {8,9,20,11},
     {12,13,14,15}
 };
 for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix[i].length; j++) {
-        System.out.print(matrix[i][j] + " ");
-        
+    if(i%2==0){
+        for (int j = 0; j < matrix[i].length; j++) {
+            System.out.print(matrix[i][j]);
+        }
+    }
+    
+        else{
+            for (int j2 = matrix[i].length-1; j2 >=0; j2--) {
+                System.out.print(matrix[i][j2]);
+            }
+        }
     }
     System.out.println();
 }
@@ -18,4 +26,3 @@ for (int i = 0; i < matrix.length; i++) {
 
     }
 
-}
