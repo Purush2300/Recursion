fd70fe6f6b9ff83d1927bd4706fb851e3c6a6189
purush2300 <reddypurushothm@gmail.com>
2026-04-01 21:16:19 +0530

    Initiala commit

--- Rec3.java
+++ Rec3.java
@@ -1,28 +1,52 @@
 public class Rec3 {
     public static void main(String[] args) {
-  int[][] matrix = {
-    {1, 2, 3,4},
-    { 5, 6,7,8},
-    {8,9,20,11},
-    {12,13,14,15}
-};
-for (int i = 0; i < matrix.length; i++) {
-    if(i%2==0){
-        for (int j = 0; j < matrix[i].length; j++) {
-            System.out.print(matrix[i][j]);
-        }
-    }
-    
-        else{
-            for (int j2 = matrix[i].length-1; j2 >=0; j2--) {
-                System.out.print(matrix[i][j2]);
+        int[][] a = {
+            {1,0,1,1},
+            {1,1,1,0},
+            {1,0,0,1},
+            {1,1,0,0}
+        };
+
+        int m = a.length;
+        int n = a[0].length;
+
+        int[][] copy = new int[m][n];
+
+        // copy original
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                copy[i][j] = a[i][j];
             }
         }
+
+        // process zeros
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(copy[i][j] == 0) {
+                    markRow(a, i);
+                    markCol(a, j);
+                }
+            }
+        }
+
+        // print result
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-    System.out.println();
+
+    public static void markRow(int[][] a, int row) {
+        for(int j = 0; j < a[0].length; j++) {
+            a[row][j] = 0;
+        }
+    }
+
+    public static void markCol(int[][] a, int col) {
+        for(int i = 0; i < a.length; i++) {
+            a[i][col] = 0;
+        }
+    }
 }
-
-
-
-    }
-
