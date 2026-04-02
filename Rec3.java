cacca08c931f10caf05766b84a577812078ad591
purush2300 <reddypurushothm@gmail.com>
2026-04-02 07:52:47 +0530

    Inittial commit

--- Rec3.java
+++ Rec3.java
@@ -1,52 +1,17 @@
 public class Rec3 {
     public static void main(String[] args) {
-        int[][] a = {
-            {1,0,1,1},
-            {1,1,1,0},
-            {1,0,0,1}
-            
-        };
-
-        int m = a.length;
-        int n = a[0].length;
-
-        int[][] copy = new int[m][n];
-
-        // copy original
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                copy[i][j] = a[i][j];
-            }
-        }
-
-        // process zeros
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(copy[i][j] == 0) {
-                    markRow(a, i);
-                    markCol(a, j);
-                }
-            }
-        }
-
-        // print result
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
+int []a={1,4,6,12,11,28};
+int l=Integer.MAX_VALUE;
+int sl=Integer.MAX_VALUE;
+for (int i = 0; i < a.length; i++) {
+    if(a[i]<l){
+        sl=l;
+        l=a[i];
     }
-
-    public static void markRow(int[][] a, int row) {
-        for(int j = 0; j < a[0].length; j++) {
-            a[row][j] = 0;
-        }
+    else if(a[i]<sl && a[i]!=l){
+        sl=a[i];
     }
-
-    public static void markCol(int[][] a, int col) {
-        for(int i = 0; i < a.length; i++) {
-            a[i][col] = 0;
-        }
+}
+System.out.println(sl);
     }
 }
