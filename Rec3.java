128e5bd83d16fe1f5faf7314fde746a5c5715998
purush2300 <reddypurushothm@gmail.com>
2026-04-02 21:33:47 +0530

    Initial commit

--- Rec3.java
+++ Rec3.java
@@ -2,12 +2,12 @@ public class Rec3 {
     public static void main(String[] args) {
 
         int []a={1,2,3,4,5};
-        int temp=a[0];
-        for (int i = 1; i < a.length; i++) {
-            a[i-1]=a[i];
+        int temp=a[a.length-1];
+        for (int i = a.length-1; i >0; i--) {
+            a[i]=a[i-1];
           
         }
-        a[a.length-1]=temp;
+        a[0]=temp;
 for (int i = 0; i < a.length; i++) {
     System.out.println(a[i]);
 }
