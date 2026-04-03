1ab77f0deb5c07888a31150852cce96d6360e7a1
purush2300 <reddypurushothm@gmail.com>
2026-04-03 21:40:32 +0530

    Initial commit

--- Rec1.java
+++ Rec1.java
@@ -1,18 +1,19 @@
 public class Rec1 {
-    static int fun(int i){
-        
-        if(i==0){
-            return 0;
-        }
-       
-        return fun(i/10)+i%10;
-       
-        
-    }
+   
     public static void main(String[] args) {
-        
-        int i=12345;
-        System.out.println(fun(i));
+    int[]a={1,1,1,0,0,1,0,0,1,1,1,0,1,1,1,0};
+    int count=0;
+    int m=Integer.MIN_VALUE;
+    for (int i = 0; i < a.length; i++) {
+        if(a[i]!=0){
+            count++;
+            m=Math.max(m,count);
+        }
+        else{
+            count=0;
+        }
+    }
+       System.out.println(m);
        
     }
 }
