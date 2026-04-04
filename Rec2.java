0798fbc2682eff158cadc7674b87e6d4a9a97de5
purush2300 <reddypurushothm@gmail.com>
2026-04-04 08:15:56 +0530

    Initial commit

--- Rec2.java
+++ Rec2.java
@@ -1,13 +1,19 @@
 public class Rec2 {
- public static void main(String[] args) {
-    int i=6;
-    int a=Indexpresentvalue(i);
-    System.out.println(a);
- }
 
- private static int Indexpresentvalue(int i) {
-   if(i==0) return 0;
-   if(i==1) return 1;
-    return Indexpresentvalue(i-1)+Indexpresentvalue(i-2);
- }   
+    public static void main(String[] args) {
+        int[] a = {1,1,1,0,1,0,0,0,1,1,1,0};
+        int count=0;
+        int k=0;
+      
+       for (int i = 0; i < a.length; i++) {
+         if(a[i]==1){
+            count++;
+            k=Math.max(k,count);
+         }
+         else{
+            count=0;
+         }
+       }
+       System.out.println(k);
+    }
 }
