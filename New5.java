2ca176c75402853fa092a93b0481f012cc22e524
purush2300 <reddypurushothm@gmail.com>
2026-04-07 07:34:20 +0530

    Initial commit

--- New5.java
+++ New5.java
@@ -1,41 +1,13 @@
 public class New5 {
     public static void main(String[] args) {
-        int[] a = {2, 4, 5, -1, -3, -4};
-
-        int[] positive = new int[a.length];
-        int[] negative = new int[a.length];
-
-        int p = 0, n = 0;
-
-        // separate elements
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] < 0) {
-                negative[n++] = a[i];   // FIXED
-            } else {
-                positive[p++] = a[i];   // FIXED
-            }
+        int[]a={7,1,5,3,6,4};
+        int mini=a[0];
+        int profit=0;
+        for (int i = 1; i < a.length; i++) {
+            int cost=a[i]-mini;
+            profit=Math.max(profit, cost);
+            mini=Math.min(mini,a[i]);
         }
-
-        // merge alternately
-        int i = 0, j = 0, k = 0;
-
-        while (i < p && j < n) {
-            a[k++] = positive[i++];
-            a[k++] = negative[j++];
-        }
-
-        // remaining elements
-        while (i < p) {
-            a[k++] = positive[i++];
-        }
-
-        while (j < n) {
-            a[k++] = negative[j++];
-        }
-
-        // print
-        for (int x : a) {
-            System.out.print(x + " ");
-        }
+        System.out.println(profit);
     }
 }
