cb63eb2d16e62b58334e43a4b6e0561d08c254de
purush2300 <reddypurushothm@gmail.com>
2026-08-02 09:57:21 +0530

    Initial commit

--- aug2/aug_2.java
+++ aug2/aug_2.java
@@ -1,6 +1,10 @@
 public class aug_2 {
     public static void main(String[] args) {
-        int[]arr={1,2,3,4,5,6,7};
+        int[]arr={0,0,0,2,0,3,5};
+        moveZeroToEnd(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
         // int k=3;
         // int n=arr.length;
         // for(int i=0;i<n;i++){
@@ -35,4 +39,16 @@ public class aug_2 {
     //     j--;
     //   }
     // }
+
+    private static void moveZeroToEnd(int[] arr) {
+       int j=0;
+       for(int i=1;i<arr.length;i++){
+        if(arr[i]!=0 && arr[j]==0){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j++;
+        }
+       }
+    }
 }
